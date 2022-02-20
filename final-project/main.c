03706792902c3197d4280761043e4d20993ee29a
David Slik <dslik@acm.org>
2022-02-19 21:15:11 -0800

    PoC for optical data transfer

--- final-project/main.c
+++ final-project/main.c
@@ -17,6 +17,16 @@
 #define POWER_OUT       9
 #define I2C_LM75_ADDR   0x77
 
+const uint32_t optical_output_table[] = {0x00000000, 0x00000080, 0x000000FF,
+                                         0x00008000, 0x00008080, 0x000080FF,
+                                         0x0000FF00, 0x0000FF80, 0x0000FFFF,
+                                         0x00800000, 0x00800080, 0x008000FF,
+                                         0x00808000, 0x00808080, 0x008080FF,
+                                         0x0080FF00, 0x0080FF80, 0x0080FFFF,
+                                         0x00FF0000, 0x00FF0080, 0x00FF00FF,
+                                         0x00FF8000, 0x00FF8080, 0x00FF80FF,
+                                         0x00FFFF00, 0x00FFFF80, 0x00FFFFFF};
+
 
 int main() {
     stdio_init_all();
@@ -61,9 +71,11 @@ int main() {
                 printf("Commands:\n");
                 printf("\"voltage\"            - Displays the current VCC rail voltage\n");
                 printf("\"temp\"               - Displays the current temperature\n");
-                printf("\"temp records count\" - Displays the current temperature\n");
-                printf("\"temp records print\" - Displays the current temperature\n");
-                printf("\"temp records clear\" - Displays the current temperature\n");
+                printf("\"temp records count\" - Displays the number of stored temperatures\n");
+                printf("\"temp records print\" - Displays all stored temperatures\n");
+                printf("\"temp records clear\" - Erases all stored temperatures\n");
+                printf("\"temp optical\"       - Transfers the current temperature optically\n");
+                printf("\"temp optical print\" - Transfers all stored temperatures optically\n");
                 printf("\"colour\"             - Changes the color of the RGB LED to a random value\n");
                 printf("\"clear\"              - Clear the screen\n");
                 printf("CTRL-C                 - Escape from command mode\n");
@@ -123,6 +135,61 @@ int main() {
             else if(strcmp(command, "clear") == 0)
             {
                 printf("\033[2J");
+                uart_command_clear();
+            }
+            else if(strcmp(command, "temp optical") == 0)
+            {
+                uint16_t    counter = 0;
+                uint8_t*   access_pointer = (uint8_t*) &temp;
+                temp = lm75_reg_read(I2C_LM75_ADDR);
+                const char* string = uint8_to_binary_string(access_pointer[2]);
+                printf("Binary representation: %s", string);
+                string = uint8_to_binary_string(access_pointer[3]);
+                printf(" %s\n", string);
+
+                printf("Optical representation: 0x%08X", optical_output_table[(access_pointer[2] & 0xF0) >> 4]);
+                printf(" 0x%08X", optical_output_table[(access_pointer[2] & 0x0F) >> 0]);
+                printf(" 0x%08X", optical_output_table[(access_pointer[3] & 0xF0) >> 4]);
+                printf(" 0x%08X\n", optical_output_table[(access_pointer[3] & 0x0F) >> 0]);
+
+                put_pixel(optical_output_table[(access_pointer[2] & 0xF0) >> 4]);
+                sleep_ms(500);
+                put_pixel(optical_output_table[(access_pointer[2] & 0x0F) >> 0]);
+                sleep_ms(500);
+                put_pixel(optical_output_table[(access_pointer[3] & 0xF0) >> 4]);
+                sleep_ms(500);
+                put_pixel(optical_output_table[(access_pointer[3] & 0x0F) >> 0]);
+                sleep_ms(500);
+
+                put_pixel(0x00000000);
+                uart_command_clear();
+            }
+            else if (strcmp(command, "temp optical print") == 0)
+            {
+                uint16_t num_records = flash_find_write_offset();
+                uint16_t counter = 0;
+                printf("Transferring %u records optically...\n", num_records);
+
+                while(counter < num_records)
+                {
+                    temp = flash_read_value(counter);
+                    uint8_t*   access_pointer = (uint8_t*) &temp;
+
+                    put_pixel(optical_output_table[(access_pointer[2] & 0xF0) >> 4]);
+                    sleep_ms(5);
+                    put_pixel(optical_output_table[(access_pointer[2] & 0x0F) >> 0]);
+                    sleep_ms(5);
+                    put_pixel(optical_output_table[(access_pointer[3] & 0xF0) >> 4]);
+                    sleep_ms(5);
+                    put_pixel(optical_output_table[(access_pointer[3] & 0x0F) >> 0]);
+                    sleep_ms(5);
+
+                    counter = counter + 1;
+                }
+
+                put_pixel(0x00000000);
+                printf("Transfer complete.\n");
+
                 uart_command_clear();
             }
             else
